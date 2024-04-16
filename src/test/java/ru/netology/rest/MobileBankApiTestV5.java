94f5cea9f615557e6b33b1203a12da9535857518
Regina <OsRigli87@mail.ru>
2024-04-16 19:27:40 +0500

    MobileBank add timeout ci

--- src/test/java/ru/netology/rest/MobileBankApiTestV5.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV5.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
+
 class MobileBankApiTestV5 {
     @Test
     void shouldReturnDemoAccounts() {
